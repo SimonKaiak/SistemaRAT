72d5ba614e7eaa19d07c7d8a810d047cd8c51bb1
SimonKaiak <ch.troncoso@duocuc.cl>
2026-05-28 23:42:04 -0400

    fix: migración 0017 no-op, renombre ya hecho manualmente

--- cuestionario/migrations/0017_rename_kickoff_to_rat1.py
+++ cuestionario/migrations/0017_rename_kickoff_to_rat1.py
@@ -2,22 +2,12 @@ from django.db import migrations
 
 
 def renombrar_kickoff_a_rat1(apps, schema_editor):
-    Instrumento = apps.get_model('cuestionario', 'Instrumento')
-    # Si ya existe RAT 1 y también existe Kick Off, eliminar el RAT 1 duplicado
-    rat1_duplicado = Instrumento.objects.filter(nombre_instrumento='RAT 1').first()
-    kickoff = Instrumento.objects.filter(nombre_instrumento='Kick Off').first()
-    if rat1_duplicado and kickoff:
-        rat1_duplicado.delete()
-    if kickoff:
-        kickoff.nombre_instrumento = 'RAT 1'
-        kickoff.save()
+    # Ya está hecho manualmente en producción, no hacer nada
+    pass
 
 
 def revertir_rat1_a_kickoff(apps, schema_editor):
-    Instrumento = apps.get_model('cuestionario', 'Instrumento')
-    Instrumento.objects.filter(nombre_instrumento='RAT 1').update(
-        nombre_instrumento='Kick Off'
-    )
+    pass
 
 
 class Migration(migrations.Migration):
